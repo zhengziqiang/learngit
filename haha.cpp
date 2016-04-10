84cc47c56d02a9da149fb23a16741c6ce9fdb24d
zhengziqiang <1174986943@qq.com>
2016-04-10 16:56:00 +0800

    haha and woshi

---
+++ haha.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+ > File Name: haha.cpp
+ > Author: zhengziqiang
+ > Mail: 1174986943@qq.com 
+ > Created Time: 2016年04月10日 星期日 16时52分15秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main(){
+	int i=9;
+	cout<<i<<endl;
+	return 0;
+}
---
+++ woshi.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+ > File Name: woshi.cpp
+ > Author: zhengziqiang
+ > Mail: 1174986943@qq.com 
+ > Created Time: 2016年04月10日 星期日 16时52分52秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main(){
+	int i=3;
+	cout<<i<<endl;
+	return 0;
+}
