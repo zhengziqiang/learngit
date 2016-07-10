ec2184655a3cbd73f013110e7abe7a93ff3aa168
zhengziqiang <1174986943@qq.com>
2016-07-10 22:53:38 +0800

    install

---
+++ cmake/hello.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: hello.cpp
+    > Author: ma6174
+    > Mail: ma6174@163.com 
+    > Created Time: 2016年07月08日 星期五 09时45分03秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"hello world"<<endl;
+	return 0;
+}
