98413cea44f5b0784c38a5df6335a29c438982b4
zhengziqiang <1174986943@qq.com>
2016-09-11 19:16:41 +0800

    添加集中随机数的获得方式

--- learnC++/random.h
+++ learnC++/random.h
@@ -7,9 +7,15 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-#define random(x)(rand()%x)
+#define random(x) (rand()%x)
+//宏的基本操作和实现
+//如果要实现其他的一些随机数,可以这样做
 int fun(){
 	int x;
 	for(x=0;x<10;x++)printf("%d\n",random(100));
+	float a=(float)rand()/((float)RAND_MAX/10);//得到0,10之间的随机浮点数
+
+	printf("浮点数a=%f",a);
+	int mn=(int)rand()%(n-m+1) + m;//得到[m,n]区间的一些整型的数
 	return 0;
 }
