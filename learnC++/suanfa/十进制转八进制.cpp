c58085024d7c977dabe8d19c4e7f691d0380642b
zhengziqiang <1174986943@qq.com>
2016-11-08 22:11:17 +0800

    algorightm

---
+++ learnC++/suanfa/十进制转八进制.cpp
@@ -0,0 +1,16 @@
+/*==============================================================================
+    > File Name: 十进制转八进制.cpp
+    > Author: zzq
+    > Mail: zhengziqiang1@gmail.com 
+    > Created Time: 2016年11月08日 星期二 19时30分24秒
+ ==========================================================================*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	cout<<oct<<n<<endl;
+	return 0;
+}
