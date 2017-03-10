d8d9740c39fe7e100a838dd2a25f4ce8538bdfce
zhengziqiang <1174986943@qq.com>
2017-03-10 12:16:31 +0800

    the first day project

---
+++ quene/alexnet/layer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <math.h>
+#include <time.h>
+#include <string>
+using namespace std;
+class Layer
+{
+private:
+    int number;
+public:
+    Layer() {}
+};
